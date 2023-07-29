6b17e1e9ca001a397aad7f8545a0fea310ec1e51
rosariovasquez <rosariovasquez@192.168.0.15>
2023-07-29 12:09:23 -0500

    new

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -21,5 +21,8 @@ public class DemoDay2 {
 	public static void add (int a, int b, int c) {
 		System.out.println(a+b+c);
 	}
+	public static void add (int a, int b, int c, int d) {
+		System.out.println(a+b+c+d);
+	}
 	
 }
